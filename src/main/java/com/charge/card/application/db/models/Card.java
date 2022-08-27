db39f4617608768bd09c065055329927e9e4bc0e
Hector Romero <hector.romero@encora.com>
2022-08-26 22:55:55 -0500

    Fixing imports

--- src/main/java/com/charge/card/application/db/models/Card.java
+++ src/main/java/com/charge/card/application/db/models/Card.java
@@ -1,6 +1,10 @@
 package com.charge.card.application.db.models;
 
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Setter
